64a7c15415d18948ca67056a10aed7d5c1905efb
Francisco Dias <frandias@gmail.com>
2016-02-02 18:46:40 -0500

    have a dot that will follow the mouse but won't do anything on click

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+var aDot;
+
 function setup() {
-  
+  createCanvas(500,500);
 }
 
 function draw() {
+  var aDot = new Dot()
+  aDot.renderDot(mouseX, mouseY)
+  
+  if (mouseIsPressed) {
+    
+  }
+}
+
+ 
+
+
+function Dot() {
+  var dotSize = 50;
+  
+  this.renderDot = function(newPosnX, newPosnY) {
+    fill(random(0,255),random(0,255),random(0,255))
+    ellipse(newPosnX, newPosnY, dotSize, dotSize);
+  }
   
 }
